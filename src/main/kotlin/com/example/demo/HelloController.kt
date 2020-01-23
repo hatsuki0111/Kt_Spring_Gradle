ab4cd61a8de3804de26450e2d2e5e4fd127410aa
hatsuki0111 <cist.b217.h.saitou@gmail.com>
2020-01-24 00:22:53 +0900

    ダミータスクの表示

--- src/main/kotlin/com/example/demo/HelloController.kt
+++ src/main/kotlin/com/example/demo/HelloController.kt
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+//8080/hello?name=Spring
+
 @RestController
 class HelloController(private val greeter: Greeter){
 
